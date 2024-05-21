99060d61ae7e60e6c793435a137aedebfe0b97c8
leekm <lkm1401@naver.com>
2024-05-21 14:03:47 +0900

    commit

--- server/repository/memberRepository.js
+++ server/repository/memberRepository.js
@@ -1,11 +1,58 @@
+import { db } from '../db/database_mysql80.js';
+
 
 /**
  * 회원 가입
  */
-export const getSignup = async (formData) => {
-  console.log('formData ---->> ', formData);
-  return {"cnt": 1};
+export const getSignup = async (formData) => {  
+  let result_rows = 0;
+  let phone1 = formData.phoneNumber1;
+  let phone2 = '';
+  let phone3 = '';
+
+  if(formData.phoneNumber2.length === 8){
+    phone2 = formData.phoneNumber2.slice(0, 4);
+    phone3 = formData.phoneNumber2.slice(4);
+  } else {
+    phone2 = formData.phoneNumber2.slice(0, 3);
+    phone3 = formData.phoneNumber2.slice(3);
+  }
+
+  const params = [
+      formData.userId,
+      formData.userPass,
+      formData.userName,
+      formData.emailId,
+      formData.emailDomain,
+      phone1.concat('-', phone2, '-', phone3),
+      formData.zipcode,
+      formData.address.concat(' ', formData.detailAddress)
+  ];
+
+  const sql = `
+      insert into shoppy_member(  user_id,
+                                  user_pass,
+                                  user_name,
+                                  email_id,
+                                  email_domain,
+                                  phone,
+                                  zipcode,
+                                  address,
+                                  signup_date
+                                )
+            values( ?, ?, ? ,? ,? ,? ,? ,? ,now()) 
+  `;
+
+  try {
+    const [result] = await db.execute(sql, params);
+    result_rows = result.affectedRows;
+  } catch (error) {
+    console.log(error);
+  }
+
+  return {"cnt": result_rows};
 }
+
 
 /**
  * 아이디 중복 체크
@@ -19,7 +66,7 @@ export const getIdCheck = (userId) => {
   } else {
     result.cnt = 0;  //사용가능
   }
-  return result;
+  return result; 
 }
 
 
